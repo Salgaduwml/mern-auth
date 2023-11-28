9e0cd95187aa8685272b0533fcfdf746ede0f57e
Malinda <salgaduwml@gmail.com>
2023-11-28 15:17:59 +0530

    mongodb conection setup

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to MongoDb");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+  console.log("Server on port 3000");
 });
