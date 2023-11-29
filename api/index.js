cdcafb4f1c508cafafca0912bd6c5c6bdf6c151f
Malinda <salgaduwml@gmail.com>
2023-11-29 14:42:09 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -14,9 +15,11 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server on port 3000");
 });
 
 app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
